de7d75fe41a49afd5783e23fbcdec62061daf62a
giakoii <khoipgse172073@fpt.edu.vn>
2025-04-14 19:25:24 +0700

    [Add][ProfileService] Init Api Ps020UpdateAddress

---
+++ ProfileService/Controllers/V1/Address/Ps020UpdateAddressRequest.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProfileService.Controllers.V1.Address;
+
+public class Ps020UpdateAddressRequest : AbstractApiRequest
+{
+    [Required(ErrorMessage = "AddressId is required")]
+    public Guid AddressId { get; set; }
+    
+    [Required(ErrorMessage = "AddressLine is required")]
+    public string AddressLine { get; set; } = null!;
+
+    [Required(ErrorMessage = "Ward is required")]
+    public string Ward { get; set; } = null!;
+
+    [Required(ErrorMessage = "City is required")]
+    public string City { get; set; } = null!;
+
+    [Required(ErrorMessage = "District is required")]
+    public string District { get; set; } = null!;
+    
+    [Required(ErrorMessage = "Province is required")]
+    public string Province { get; set; } = null!;
+}
